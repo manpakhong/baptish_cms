13fcc3630a16fc7adb91b757ea2f5057391f55ae
manpakhong <manpakhong@192.168.1.208>
2018-07-09 20:29:38 +0800

    init commit

---
+++ modules/clinicalnote-web/src/main/java/hk/org/hkbh/cms/outpatient/portlet/constants/ClinicalNotePortletKeys.java
@@ -0,0 +1,10 @@
+package hk.org.hkbh.cms.outpatient.portlet.constants;
+
+/**
+ * @author manpakhong
+ */
+public class ClinicalNotePortletKeys {
+
+	public static final String ClinicalNote = "ClinicalNote";
+
+}
---
+++ modules/clinicalnote-web/src/main/java/hk/org/hkbh/cms/outpatient/portlet/portlet/ClinicalNotePortlet.java
@@ -0,0 +1,29 @@
+package hk.org.hkbh.cms.outpatient.portlet.portlet;
+
+import hk.org.hkbh.cms.outpatient.portlet.constants.ClinicalNotePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author manpakhong
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=clinicalnote-web Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ClinicalNotePortletKeys.ClinicalNote,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ClinicalNotePortlet extends MVCPortlet {
+}
